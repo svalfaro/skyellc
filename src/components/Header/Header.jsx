711c65863a525b2cce5ff7348c12c56f2d0d62fc
Sheila Alfaro <sa2223@njit.edu>
2024-04-04 20:33:29 -0400

    updated.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { NAV_LINKS } from '/src/components/Header/headerLinks.js';
+import { NAV_LINKS } from './components/Header/headerLinks.js';
 import './Header.css';
 
 export default function Header() {
